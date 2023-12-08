a51406b1e8126e62c47525ca6e5e505502f07a78
Yash Manish Doshi <143429193+YashDoshi29@users.noreply.github.com>
2023-12-08 17:23:31 -0500

    Add files via upload

--- Greenhouse.py
+++ Greenhouse.py
@@ -32,3 +32,8 @@ data.columns
 data=data.dropna()
 data.info()
 # %%
+data.columns
+#%%
+cols = ['ISO2', 'CTS_Full_Descriptor', 'CTS_Code', 'F1986', 'F1987', 'F1988', 'F1989', 'F1990', 'F1991', 'F1992', 'F1993', 'F1994', 'F1995', 'F1996', 'F1997', 'F1998', 'F1999', 'F2000']
+data = data.drop(cols, axis=1)
+
